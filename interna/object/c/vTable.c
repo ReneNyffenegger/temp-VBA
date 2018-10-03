dfd8b6fa556cc2efd2e8132d1dafb41b6fdcea51
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-10-03 21:46:04 +0200

    n/a

--- interna/object/c/vTable.c
+++ interna/object/c/vTable.c
@@ -105,6 +105,10 @@ struct IUnkown_vTable *vTable;
 struct vb_object {
 
   struct IUnkown_vTable *vTbl;
+
+  long  b[6];
+
+  struct IUnkown_vTable * iunknown;
   ULONG  refCnt;
 
 };
@@ -200,15 +204,17 @@ __stdcall HRESULT QueryInterface(void* this, const REFIID iid, void** ppv) {
 
 __stdcall HRESULT AddRef(void* this) {
 
+  struct vb_object *vb_obj = (struct vb_object*) this;
+
 //PUSH_REGISTERS
 
-  writeToFile("AddRef, this = %p", this);
+//writeToFile("AddRef, this = %p", this);
 
   HRESULT r = AddRefOrig(this);
 
 //POP_REGISTERS
 
-  writeToFile("AddRef, r = %d", r);
+  writeToFile("AddRef, r = %d, refCnt = %d", r, vb_obj->refCnt);
 
   return r;
 
